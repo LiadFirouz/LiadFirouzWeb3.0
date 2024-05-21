9bb21c68359077dae2aad62938674a5b228a369a
Liad Firouz <Liad.Firouz@gmail.com>
2024-05-21 08:35:42 +0300

    Second commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+/* Write your code here:*/
+//Step 1: Render the home page "/" index.ejs
+app.get("/", (req, res)=>{
+  res.render("index.ejs");
+});
+
+//Step 2: Make sure that static files are linked to and the CSS shows up.
+app.use(express.static("public"));
+
+/*Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs*/
+  app.get("/cv", (req, res)=>{
+    res.render("cv.ejs");
+  });
+  
+  app.get("/contact", (req, res)=>{
+    res.render("contact.ejs");
+  });
+  
+
+//Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
